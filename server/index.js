0228916c7280732cbd1d5e93c6aff7f4b306e027
srikanthP1106 <srikanthparuchuri40@gmail.com>
2026-07-27 23:41:57 +0530

    Use dynamic PORT for deployment

--- server/index.js
+++ server/index.js
@@ -171,6 +171,8 @@ res.json({
 }
 });
 
-app.listen(5000, () => {
-  console.log("Server started on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
